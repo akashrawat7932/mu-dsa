2d42ad7eda6316b1c905c3989baf715d33264465
Akash Rawat <akashrawatiglas0@gmail.com>
2026-09-01 23:20:26 +0530

    First commit

---
+++ condition.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+      int marks;
+cout<<"enter your marks"<<endl;
+cin>>marks;
+  
+    if(marks>90)
+    {
+cout<<"grade A+";
+    }
+    else{
+        cout<<"fail";
+    }
+    
+    return 0;
+}
